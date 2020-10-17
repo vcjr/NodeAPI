166aa0c2023fddbde512a24c5c9e9e56ee7a4589
vcjr <victorrcrispinjr@gmail.com>
2020-10-16 22:08:53 -0400

    Add code to return game information from json file

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 // Requireing the http module included with Node
 const http = require("http");
+const gameData = require("./data/upcomingVideoGames.json");
+
 
 // Seperating out the variables
 // Creating and saving the server as a constant to be able to make this more modular
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader("Content-Type", "text/html");
+//   res.write("<h1>Hello World</h1>");
+//   res.end();
+// });
+
+// Shorter way of initializing header info
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.write("<h1>Hello World</h1>");
-  res.end();
+  res.writeHead(200, {'Content-Type' : 'application/json'});
+  res.end(JSON.stringify(gameData));
+
 });
 
 // Setting a default port or taking in the default enviroment port
